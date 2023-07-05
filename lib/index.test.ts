86344db91203cea66cd1453fe3fd8850c19eb13d
Paul Adams <ghusiyy@gmail.com>
2023-07-05 14:38:23 -0700

    update

--- lib/index.test.ts
+++ lib/index.test.ts
@@ -1,5 +1,5 @@
 import { assertEquals } from "https://deno.land/std@0.191.0/testing/asserts.ts";
-import { createBiographicalEntry, nameFromSymbol } from "./index.ts";
+import { nameFromSymbol } from "./index.ts";
 import { biographicalEntries } from "./entries.ts";
 
 Deno.test("parseBiographicalEntry", () => {
