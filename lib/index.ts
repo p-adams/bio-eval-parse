f31c52ac4420fa178b514ce3521b7ec91b6cc1de
Paul Adams <ghusiyy@gmail.com>
2023-07-10 15:57:21 -0700

    name symbols for Bukhari

--- lib/index.ts
+++ lib/index.ts
@@ -1,7 +1,15 @@
-export const map: Lib.SymbolMap = { م: "مسلم" };
+export const symbolMap: Lib.SymbolMap = {
+  م: "مسلم",
+  خ: "البخاري في صحيحه",
+  خت: "البخاري معلقا",
+  بخ: "البخاري في الأدب المفرد",
+  عخ: "البخاري في خلق أفعال العباد",
+  ر: "البخاري في جزءالقراءة",
+  ي: "البخاري في رفع اليدين",
+};
 
 export const nameFromSymbol = (symbol: string): string => {
-  return map[symbol];
+  return symbolMap[symbol];
 };
 
 function _parseBiographicalEntry(
